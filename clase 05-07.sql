b3f180882428700f93b1ac57e3ab63dc88ea349f
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-07-05 17:55:07 -0300

    agrega tarea de 5-7

---
+++ clase 05-07.sql
@@ -0,0 +1,9 @@
+/* CENTRO CULTURAL */
+
+CREATE TABLE epocas(
+    nombre VARCHAR(50) NOT NULL,
+    caracteristicas VARCHAR(50) NOT NULL,
+    domicilio VARCHAR(70) NOT NULL,
+    telefono INT(10),
+    PRIMARY KEY(nombre)
+)
